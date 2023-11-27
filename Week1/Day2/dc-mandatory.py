306aee7bdb7d5374b6a9fb41db6d5c565ebe20a6
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-11-27 12:30:07 +0200

    add commit

--- Week1/Day2/dc-mandatory.py
+++ Week1/Day2/dc-mandatory.py
@@ -1,1 +1,71 @@
 
+
+
+user_str=input('give me a strinf 10 chr long:')
+
+if len (user_str)<10:
+    print ('string not long enoth') 
+elif len (user_str)>10:
+    print('string too long ')
+else:
+    print('perfect string') 
+
+  #2___
+    print(f'first characters "{string[0]}" ,the last  characters "{string[-1] }"')
+
+
+    '''string = str(input("enter 10 characters long string"))
+
+if len(string) < 10: 
+    print('string  not long enough')
+elif len(string) > 10: 
+    print('string too long ')
+else:
+    print('perfect string') 
+
+  #2___
+    print(f'first characters "{string[0]}" ,the last  characters "{string[-1] }"')
+
+  #3___
+    #for i in range(len(string)):
+    #   print(string[:i+1])
+  
+  
+output_str= ''
+for char in string:
+    output_str
+
+
+
+
+  #4___
+    import random
+    s_list=list(string)
+    random.shuffle(s_list)
+    shuffled_string=''.join(s_list)
+    print(shuffled_string)
+'''
+
+
+list=[5,10,15,20,25,50,20]
+for i in range(len(list)):
+  if list[i]==20:
+    list[i]=200
+    break
+print(list)
+
+
+
+
+
+
+my_set= set()
+my_set2= {60,14,"Morty"}
+my_set.add("Rick")
+my_set.add('Morty')
+my_set.add("Rick")
+print(my_set2)
+inter=my_set.intersection(my_set2)
+print(inter)
+#dif=my_set.diference(my_set2)
+#print(dif) ???????
