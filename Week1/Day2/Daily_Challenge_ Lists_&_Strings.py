ec36c9c8772e217c9353635e6b45d9dccc8b562c
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-11-28 08:35:17 +0200

    Update Daily_Challenge_ Lists_&_Strings.py

--- Week1/Day2/Daily_Challenge_ Lists_&_Strings.py
+++ Week1/Day2/Daily_Challenge_ Lists_&_Strings.py
@@ -1,1 +1,36 @@
+# Daily Challenge : Lists & Strings_____________________________________________________________________________________________________________________________________
+
+#________Challenge 1 ________
+
+
+
+number = int(input(('please,enter yor number:')))
+length =int(input(('and enter length:')))
+num_list=[]
+
+
+
+for i in range(1,(length+1)):
+    current_num = number*i
+    num_list.append(current_num)
+
+
+print(f'number: {number} - length {length } ➞ {num_list}')
+
+#___________________________
+
+
+
+#________Challenge 2 ________
+
+
+string = str(input(('please,enter yor string:')))
+new_string = ''
+numbers=1
+
+for i in string:
+    if i not in new_string:
+        new_string += i
+
+print(f" user's word : '{string}'  ➞  '{new_string}'")
 
