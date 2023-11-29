81ce794bd0156064b13b218b708f207ffcc2eada
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-11-29 11:05:54 +0200

    Challenge2

--- Week1/Day3/Daily_Challenge_Dictionaries.py
+++ Week1/Day3/Daily_Challenge_Dictionaries.py
@@ -11,5 +11,38 @@ for index, letter in enumerate(word):
 
 print(letter_indexes)
 
-#_________________________________
+#______________________
+
+
+#______Challenge 2____
+#1
+items_purchase = {
+  "Water": "$1",
+  "Bread": "$3",
+  "TV": "$1,000",
+  "Fertilizer": "$20"
+}
+can_afford = []
+
+
+wallet = "$300"
+wallet_amount = int(wallet.replace('$', '').replace(',', ''))
+
+for item, price in items_purchase.items():
+    price_amount = int(price.replace('$', '').replace(',', ''))
+
+    if price_amount <= wallet_amount :
+        can_afford.append(item)
+        wallet_amount -= price_amount
+
+print(can_afford)
+#2
+can_afford.sort()
+print(can_afford)
+
+ #3
+if len(can_afford) == 0:
+    print('Nothing')
+else: print(can_afford)
+
 
