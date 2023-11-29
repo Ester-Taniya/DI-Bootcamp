bd7e72567def5b34224784ca236c04b81f36fcd0
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-11-29 10:17:06 +0200

    Challenge 1

--- Week1/Day3/Daily_Challenge_Dictionaries.py
+++ Week1/Day3/Daily_Challenge_Dictionaries.py
@@ -1,1 +1,15 @@
+'''____________Daily Challenge: Dictionaries_____________________________________________________________'''
+
+#_______Challenge 1 ______________
+word = input("Enter a word: ")
+
+letter_indexes = {}
+for index, letter in enumerate(word):
+    letter = str(letter)
+    letter_indexes.setdefault(letter, [])
+    letter_indexes[letter].append(index)
+
+print(letter_indexes)
+
+#_________________________________
 
