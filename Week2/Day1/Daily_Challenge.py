a7312bc653da876cb8b7bdbb61c6dd17fa1ea4ba
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-04 09:58:32 +0200

    Update Daily_Challenge.py

--- Week2/Day1/Daily_Challenge.py
+++ Week2/Day1/Daily_Challenge.py
@@ -1,29 +1,53 @@
-''___Daily Challenge_____________________________________________________________________________________________________________________________________''''
+'''___Daily Challenge_____________________________________________________________________________________________________________________________________''''
+#1
+class Farm:
+    #2
+    def __init__(self, name):
 
-#____   _____
-#___________________________
-
-
-#____  ____________________
-#__________________________
-
-
-
-
-#____  ____________________
-#__________________________
+        self.name = name
+        self.animals = {}
+    #3 minimum 1 metodh
 
 
-#____  ____________________
-#__________________________
+    #4
+    def add_animal(self,animal,count=1):
+
+        if animal not in self.animals:
+            self.animals[animal]=count
+        else: self.animals[animal] += 1
+
+
+#5-6Bonus:
+    def get_info(self):
+        print(f"{self.name}s farm'\n")
+        for key,value in self.animals.items():
+            print(f'{key} : {value}\n')
+        print('    E-I-E-I-0!')
+
+
+#Expand The Farm #1
+    def get_animal_types(self):
+        return sorted(self.animals.keys())
+#Expand The Farm #2
+
+    def get_short_info(self):
+        animal_types = self.get_animal_types()
+        print(f"{self.name}’s farm has {', '.join(animal_types)}")
 
 
 
 
-#____  ____________________
-#__________________________
+macdonald = Farm("McDonald")
+macdonald.add_animal('cow',5)
+macdonald.add_animal('sheep')
+macdonald.add_animal('sheep')
+macdonald.add_animal('goat', 12)
+(macdonald.get_info())
+
+#Expand The Farm
+print(macdonald.get_animal_types())
+macdonald.get_short_info()
 
 
-#____  ____________________
 #__________________________
 
