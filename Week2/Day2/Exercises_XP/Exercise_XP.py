13c18be91cc86de37490ccfcaf69993f1b4c7999
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-05 09:04:24 +0200

    canged

--- Week2/Day2/Exercises_XP/Exercise_XP.py
+++ Week2/Day2/Exercises_XP/Exercise_XP.py
@@ -34,9 +34,9 @@ class Siamese(Cat):
     def sing(self, sounds):
         return f'{sounds}'
 
-cat1=Bengal('Salem',11)
-cat2=Chartreux("Tom",2)
-cat3=Siamese('Wiskers',5)
+cat1 = Bengal('Salem',11)
+cat2 = Chartreux("Tom",2)
+cat3 = Siamese('Wiskers',5)
 
 #2
 
@@ -50,8 +50,9 @@ sara_pets.walk()
 
 
 #____Exercise 2 : Dogs _______________
+
 class Dog():
-    def __init__(self, name, age,weight):
+    def __init__(self, name, age, weight):
 
         self.name = name
         self.age = age
@@ -73,17 +74,18 @@ class Dog():
         elif self_run_speed < other_run_speed:
             print(f'{other_dog.name} won the fight!')
         else: print('It a draw!')
+
+
+
+dog1 = Dog('Rex', 2, 6.5)
+dog2 = Dog('Jack', 7, 12.8)
+dog3 = Dog('Moe', 12, 3.0)
+
 if __name__ == '__main__':
-
-
-    dog1 = Dog('Rex', 2, 6.5)
-    dog2 = Dog('Jack', 7, 12.8)
-    dog3 = Dog('Moe', 12, 3.0)
-
 
     print(dog1.name)
     Dog.bark(dog3)
-    Dog.fight(dog1,dog2)
+    Dog.fight(dog1, dog2)
 
 
 
--- Week2/Day2/Exercises_XP/XP_2.py
+++ Week2/Day2/Exercises_XP/XP_2.py
@@ -4,16 +4,28 @@ from Exercise_XP import Dog
 
 class PetDog(Dog):
 
-    def __init__(self, trained = False):
-        self.trained = trained
+    def __init__(self, name, age, weight):
+        super().__init__(name, age, weight)
+        self.trained = False
 
     def train(self):
-        Dog.bark
-        self.trained=True
+        print(super().bark())
+        self.trained = True
+        print(f'{self.name} now is trained!')
 
-    def play(self,*args):
-        dog_names=[*args] 
-        print(f'{dog_names} all play together')
+
+    def play(self, *args):
+        dog_names = [dog.name for dog in args]
+        print(f'{", ".join(dog_names)} all play together')
+
+dog4 = PetDog('Rassel', 8, 3.4)
+dog5 = PetDog('Mojo',1 , 1.3)
+
+print(dog4.name)
+PetDog.train(dog4)
+PetDog.play(dog4, dog5)
+
+
 
 
 
