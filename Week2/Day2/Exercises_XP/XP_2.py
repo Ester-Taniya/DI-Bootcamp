71cd4ead9ab3000b33f1a2ff3586ad45b61caecd
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-05 08:10:49 +0200

    canges

--- Week2/Day2/Exercises_XP/XP_2.py
+++ Week2/Day2/Exercises_XP/XP_2.py
@@ -1,1 +1,26 @@
+'''___Exercises XP 2_____________________________________________________________________________________________________________________________________'''
+
+from Exercise_XP import Dog
+
+class PetDog(Dog):
+
+    def __init__(self, trained = False):
+        self.trained = trained
+
+    def train(self):
+        Dog.bark
+        self.trained=True
+
+    def play(self,*args):
+        dog_names=[*args] 
+        print(f'{dog_names} all play together')
+
+
+
+
+
+
+
+          
+
 
