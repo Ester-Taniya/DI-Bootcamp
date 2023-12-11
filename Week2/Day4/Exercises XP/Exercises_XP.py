07e141e95ebc458e273584b5ed5c7b24c3895620
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-11 15:31:56 +0200

    commit

--- Week2/Day4/Exercises XP/Exercises_XP.py
+++ Week2/Day4/Exercises XP/Exercises_XP.py
@@ -1,10 +1,11 @@
 '''___Exercises XP_____________________________________________________________________________________________________________________________________'''
 
 #____Exercise 1 – Random Sentence Generator   _____
-
+import os
+dir_path = os.path.dirname(os.path.realpath(__file__))
 def get_words_from_file():
-    with open('sowpods1.txt', 'r') as file:
-        text = file.read().split()
+    with open(dir_path + '/sowpods1.txt', 'r') as file:
+        text = file.read().split('\n')
     print(text)
 get_words_from_file()
 
