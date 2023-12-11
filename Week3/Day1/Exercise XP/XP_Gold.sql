7a4ad1e126fab580a97ddc5435fab23d7649409a
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-11 10:18:02 +0200

    commit

---
+++ Week3/Day1/Exercise XP/XP_Gold.sql
@@ -0,0 +1,9 @@
+
+select first_name, last_name, birth_date from students order by last_name limit 4;
+select first_name, last_name, birth_date from students order by birth_date DESC limit 1;
+select  first_name, last_name, birth_date from students OFFSET 2 limit 3;
+
+
+
+
+
