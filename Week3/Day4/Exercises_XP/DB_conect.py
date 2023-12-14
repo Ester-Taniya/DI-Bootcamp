10bb3e8a4f2c75270da1f942c0e902b48696d075
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-14 20:34:44 +0200

    commit

--- Week3/Day4/Exercises_XP/DB_conect.py
+++ Week3/Day4/Exercises_XP/DB_conect.py
@@ -1,10 +1,8 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
-  
 
 def get_db_connection():
-   
     load_dotenv()
     db_name = os.getenv('DB_name')
     db_user = os.getenv('DB_user')
@@ -12,17 +10,15 @@ def get_db_connection():
     db_host = os.getenv('DB_host')
     db_port = os.getenv('DB_port')
 
-
     conn = psycopg2.connect(
         dbname=db_name,
         user=db_user,
         password=db_password,
         host=db_host,
-        port=db_port,)
+        port=db_port
+    )
+
     return conn
 
-
-
 if __name__ == "__main__":
-
-    
+    get_db_connection()
--- Week3/Day4/Exercises_XP/menu_manager.py
+++ Week3/Day4/Exercises_XP/menu_manager.py
@@ -1,13 +1,36 @@
-from menu_item import Menu_Items
+from DB_conect import get_db_connection
 
+conn = get_db_connection()
+cursor = conn.cursor()
     
+
+
 class MenuManager:
-    @classmethod
-    def get_by_name(cls, name):
-        cursor.execute("SELECT * FROM Menu_Items WHERE item_name = %s", (name,))
-        result = cursor.fetchone()
-        if result:
-            return MenuItem(result[1], result[2])
-        return None
 
+    def __init__(self, item_name):
+        self.item_name = item_name
     
+    @classmethod
+    def get_by_name(cls, item_name):
+        cursor.execute("SELECT * FROM Menu_Items WHERE item_name = %s", (item_name,))
+
+        row = cursor.fetchall()
+        if row:
+            print(row[0][1], row[0][2])
+        else:
+            print("Item not found.")
+
+    @classmethod
+    def all_items(cls):
+        cursor.execute("SELECT * FROM Menu_Items")
+
+        rows = cursor.fetchall()
+        for row in rows:
+            print(list(row))
+        
+
+item=MenuManager(1)
+item.get_by_name('test3')
+
+   
+
