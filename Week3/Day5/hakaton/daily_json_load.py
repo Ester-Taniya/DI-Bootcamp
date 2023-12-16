ad5f527c19cafdca4c50174cd033081f67c24d84
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-16 21:27:00 +0200

    commit

--- Week3/Day5/hakaton/daily_json_load.py
+++ Week3/Day5/hakaton/daily_json_load.py
@@ -6,31 +6,34 @@ from datetime import datetime
 
 def fetch_and_save_data():
     url = "https://api.tzevaadom.co.il/alerts-history/"
+    
+    # Replace 'YYYY-MM-DD' with actual start and end dates
     params = {
-        'start_date': 'YYYY-MM-DD',
-        'end_date': 'YYYY-MM-DD',
+        'start_date': '2023-01-01',
+        'end_date': '2023-12-31',
         # Other parameters as needed
     }
 
     response = requests.get(url, params=params)
 
     if response.status_code == 200:
-        filename = "data_.json"
+        # Generate a filename with the current date
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        filename = f"data_{current_date}.json"
 
-        #  new file saver
+        # Save the data to a JSON file
         with open(filename, 'w', encoding='utf-8') as file:
             file.write(response.text)
         print(f"Data saved to {filename}")
     else:
+        # Log details about the error
         print(f"Error {response.status_code}: Unable to fetch data.")
+        print(response.text)  # Print the response content for debugging
 
+# Schedule to run at 23:59 every day
+schedule.every().day.at("21:27").do(fetch_and_save_data)
 
-
-
-# run function in  23:59
-schedule.every().day.at("21:20").do(fetch_and_save_data)
-
-# unending  cycle:
+# Unending cycle
 while True:
     schedule.run_pending()
     time.sleep(1)
