923949c7a5e6ae61428db24ccaa69466e309f24f
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-16 15:14:49 +0200

    commit

---
+++ Week3/Day5/hakaton/dayly_jason_load.py
@@ -0,0 +1,35 @@
+import requests
+import json
+import schedule
+import time
+from datetime import datetime
+
+def fetch_and_save_data():
+    url = "https://api.tzevaadom.co.il/alerts-history/"
+    params = {
+        'start_date': 'YYYY-MM-DD',
+        'end_date': 'YYYY-MM-DD',
+        # Other parameters as needed
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        # name generator
+        current_date = datetime.now().strftime("%Y-%m-%d")
+        filename = f"data_{current_date}.json"
+
+        # Сnew file saver
+        with open(filename, 'w', encoding='utf-8') as file:
+            file.write(response.text)
+        print(f"Data saved to {filename}")
+    else:
+        print(f"Error {response.status_code}: Unable to fetch data.")
+
+# run function in  23:59
+schedule.every().day.at("15:14").do(fetch_and_save_data)
+
+# unending  cycle:
+while True:
+    schedule.run_pending()
+    time.sleep(1)
