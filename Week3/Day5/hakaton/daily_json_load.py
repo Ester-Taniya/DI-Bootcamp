9a85427d91db509a7e412fe7bd24420583dfc8fa
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-16 23:26:15 +0200

    commit

--- Week3/Day5/hakaton/daily_json_load.py
+++ Week3/Day5/hakaton/daily_json_load.py
@@ -4,7 +4,7 @@ import schedule
 import time
 from datetime import datetime
 
-def fetch_and_save_data():
+def fetch_data():
     url = "https://api.tzevaadom.co.il/alerts-history/"
     
     # Replace 'YYYY-MM-DD' with actual start and end dates
@@ -17,21 +17,19 @@ def fetch_and_save_data():
     response = requests.get(url, params=params)
 
     if response.status_code == 200:
-        # Generate a filename with the current date
-        current_date = datetime.now().strftime("%Y-%m-%d")
-        filename = f"data_{current_date}.json"
-
-        # Save the data to a JSON file
-        with open(filename, 'w', encoding='utf-8') as file:
-            file.write(response.text)
-        print(f"Data saved to {filename}")
+        # Parse the JSON response
+        data = json.loads(response.text)
+        
+        # Process the data as needed
+        print("Data fetched successfully.")
+        print(data)
     else:
         # Log details about the error
         print(f"Error {response.status_code}: Unable to fetch data.")
         print(response.text)  # Print the response content for debugging
 
 # Schedule to run at 23:59 every day
-schedule.every().day.at("22:26").do(fetch_and_save_data)
+schedule.every().day.at("22:34").do(fetch_data)
 
 # Unending cycle
 while True:
