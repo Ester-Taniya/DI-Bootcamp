5a21e113378b15661edec0dec2c0cadfd8f79dc2
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-17 18:12:10 +0200

    Update alert_each_city.py

--- Week3/Day5/hakaton/alert_each_city.py
+++ Week3/Day5/hakaton/alert_each_city.py
@@ -1,45 +1,33 @@
 from DB_conect import get_db_connection
 
-conn = get_db_connection()
-cursor = conn.cursor()
-    
+class City:
+    # Connect to the database
+    conn = get_db_connection()
 
+    def __init__(self, en_name):
+        self.en_name = en_name
 
-class MenuManager:
+    def show(self):
+        # Use a context manager for handling the database connection
+        with self.conn.cursor() as cursor:
+            cursor.execute(
+                "SELECT city_id FROM city WHERE en_name = %s", (self.en_name,))
 
-    def __init__(self, city_name):
-        self.city_name = city_name
-    
+            row = cursor.fetchall()
+            if row:
+                return(row[0][0])
+            else:
+                return None #("Item not found")
 
-    @classmethod
-    def get_by_name(cls, city_name):
-        cursor.execute("SELECT * FROM   WHERE en_name = %s", (city_name,))
+        self.conn.commit()
 
-        row = cursor.fetchall()
-        if row:
-            print(row[0][1], row[0][2])
-        else:
-            print("The city not found.")
-        
+# Create an instance of City
+city1 = City('Tel Aviv')
 
-        '''
-        
+# Call the show method on the instance
+city1.show()
 
-    @classmethod
-    def all_items(cls):
-        cursor.execute("SELECT * FROM Menu_Items")
-
-        rows = cursor.fetchall()
-        for row in rows:
-            print(list(row))
-'''
-           
-
-                    
-
-
-
-
-
+# Close the database connection outside the class when done
+city1.conn.close()
 
 
