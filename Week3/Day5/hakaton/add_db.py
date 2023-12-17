26f4febfd1bf8a4542fb77688c8c9a38f63a17ca
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-17 08:14:52 +0200

    commit

--- Week3/Day5/hakaton/add_db.py
+++ Week3/Day5/hakaton/add_db.py
@@ -1,20 +1,3 @@
 from DB_conect import get_db_connection
 
-conn = get_db_connection()
-cursor = conn.cursor()
 
-class AlertItem:
-    def __init__(self, id, city, time):
-        self.id = id
-        self.City = city
-        self.Time = time
-
-    
-
-    def save(self):
-        # Insert data into the "alerts" table
-        cursor.execute(
-            "INSERT INTO alerts (id, city, time) VALUES (%s, %s, %s)",
-            (self.id, self.City, self.Time),
-        )
-        conn.commit()
