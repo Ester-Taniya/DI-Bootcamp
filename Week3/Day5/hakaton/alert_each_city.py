b875b2d64c86c076576e6152e2cd7d228e244e23
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-17 14:40:14 +0200

    commit

--- Week3/Day5/hakaton/alert_each_city.py
+++ Week3/Day5/hakaton/alert_each_city.py
@@ -1,86 +1,38 @@
-import requests
-import json
-import schedule
-import time
-from datetime import datetime
-
 from DB_conect import get_db_connection
 
 conn = get_db_connection()
 cursor = conn.cursor()
+    
 
 
-def fetch_data():
-    url = "https://api.tzevaadom.co.il/alerts-history/"
+class MenuManager:
 
-    # Set start_date and end_date to the current date
-    current_date = datetime.now().strftime("%Y-%m-%d")
-    params = {
-        'start_date': current_date,
-        'end_date': current_date,
-    }
+    def __init__(self, city_name):
+        self.city_name = city_name
+    
 
-    response = requests.get(url, params=params)
+    @classmethod
+    def get_by_name(cls, city_name):
+        cursor.execute("SELECT * FROM   WHERE en_name = %s", (city_name,))
 
-    if response.status_code == 200:
-        # Return the fetched data
-        return json.loads(response.text)
-    else:
-        # Log details about the error
-        print(f"Error {response.status_code}: Unable to fetch data.")
-        print(response.text)  # Print the response content for debugging
+        row = cursor.fetchall()
+        if row:
+            print(row[0][1], row[0][2])
+        else:
+            print("The city not found.")
+        
 
+        '''
+        
 
-class AlertItem:
-    def __init__(self, id, city, time):
-        self.id = id
-        self.City = city
-        self.Time = time
+    @classmethod
+    def all_items(cls):
+        cursor.execute("SELECT * FROM Menu_Items")
 
-    def save(self):
-        # Insert data into the "alerts" table
-        cursor.execute(
-            "INSERT INTO alerts (id, city, time) VALUES (%s, %s, %s)",
-            (self.id, self.City, self.Time),
-        )
-        conn.commit()
-
-
-# Function to format time
-def format_time(entry):
-    for alert in entry['alerts']:
-        # Format time as real date-time
-        dt_object = datetime.fromtimestamp(alert['time'])
-        alert['time'] = dt_object.strftime("%Y-%m-%d %H:%M:00")
-
-    return entry
-
-
-def main():
-    json_data = fetch_data()
-    formatted_data = [format_time(entry) for entry in json_data]
-
-    for entry in formatted_data:
-        for alert in entry['alerts']:
-            for city in alert['cities']:
-                ID = entry['id']
-                City = city.split(' -')[0]
-                Time = alert['time']
-                item = AlertItem(ID, City, Time)
-                item.save()
-
-    conn.close()
-
-
-# Schedule to run at 23:59 every day
-schedule.every().day.at("23:59").do(main)
-
-# Unending cycle
-while True:
-    schedule.run_pending()
-    time.sleep(1)
-
-
+        rows = cursor.fetchall()
+        for row in rows:
+            print(list(row))
+'''
            
 
                     
---
+++ Week3/Day5/hakaton/main_program_body.py
@@ -0,0 +1,18 @@
+from DB_conect import get_db_connection
+
+
+
+# Greeting the user
+print("Hello! Welcome to the Alert Predictor!")
+print("DISCLAIMER: This program provides the probability of an air raid based on alarm history.")
+print("ALL INFORMATION IS ADVISORY.")
+
+# Asking for the city name
+city_name = input("Enter the name of your city: ")
+
+
+
+# Displaying a greeting using the entered city
+print("You entered the city {city_name}.")
+
+
