606d8054a94d5449caf06649dbe5d1fc15735aa8
Tatiana Bobyleva <109293770+Ester-Taniya@users.noreply.github.com>
2023-12-18 10:51:46 +0200

    commit

--- Week3/Day5/hakaton/main_program_body.py
+++ Week3/Day5/hakaton/main_program_body.py
@@ -1,5 +1,5 @@
-from alert_each_city import City 
 
+from alert_each_city import City
 
 # Greeting the user
 print("Hello! Welcome to the Alert Predictor!")
@@ -7,40 +7,45 @@ print("DISCLAIMER: This program provides the probability of an air raid based on
 print("ALL INFORMATION IS ADVISORY.")
 
 # Asking for the city name
-city_name = input("Enter the name of your city: ")
+city_name = input("Enter the name of your city: ").capitalize()
 
 city = City(city_name, 0, 0)
 
-
 city_id = city.show_city()
 if city_id is not None:
-    print(f'let see what hapen in {city_name}')
-    time= int(input("Enter number (0-24) of hour what you want to cheack :  "))
+    print(f"Let's see what happens in {city_name}")
+    time = input("Enter the hour (0-24) you want to check: ")
+
+    while not time.isdigit() or int(time) not in range(0, 24):
+        print("Sorry, wrong input. Please enter a valid hour (0-24).")
+        time = input("Enter the hour (0-24) you want to check: ")
+
+    time = int(time)
 elif city_id is None:
-    print(f'sorty,the {city_name} not finde maby you mean :')
+    print(f"Sorry, {city_name} not found. Maybe you meant:")
     city.show_list_cities()
-else:print('sorty,somfing wrong try in next time')
+else:
+    print("Sorry, something went wrong. Try again next time.")
 
-while time not in range(0,24):
-    print('sorry wrong input')
-    time=int(input('enter a hour number :'))
+city_check = City(city_name, city_id, time)
 
+try:
+    total_alerts_in_hour = city_check.all_alerts_in_cities()
+    city_hour_alerts = city_check.hour_alerts_in_city()
 
-city_cheak=City(city_name, city_id, time)
+    percentage_alerts_outcomes = (city_hour_alerts / total_alerts_in_hour) * 100
 
-total_alerts_in_hour = city_cheak.all_alerts_in_cities()
-city_hour_alerts=city_cheak.hour_alerts_in_city()
-
-percentage_alerts_outcomes = (city_hour_alerts / total_alerts_in_hour) * 100
-
-if 10 < percentage_alerts_outcomes< 30:
-    print('its will be prity good hour')
-elif 30 < percentage_alerts_outcomes < 50:
-    print('its will be quite a quiet hour')
-elif 50 < percentage_alerts_outcomes < 70:
-    print('there is a possibility of an air raid')
-else: print('be careful,maybe you should consider staying home')
-
-
+    if 10 < percentage_alerts_outcomes < 30:
+        print("It will be a pretty good hour.")
+    elif 30 < percentage_alerts_outcomes < 50:
+        print("It will be quite a quiet hour.")
+    elif 50 < percentage_alerts_outcomes < 70:
+        print("There is a possibility of an air raid.")
+    else:
+        print("Be careful, maybe you should consider staying home.")
+except Exception as e:
+    print(f"An error occurred: {e}")
+finally:
+    city_check.conn.close()
 
  
